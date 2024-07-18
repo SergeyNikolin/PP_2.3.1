7c2ba2b2994539b79df1e5243c6556a0f080dee5
Nikolin Sergey <nikolinsergei@mail.ru>
2024-07-18 12:36:31 +0300

    Добавлен класс Car, описывающий параметры автомобиля. Добавлены CarsController и cars.html, формирующие и выводящие в окно браузера таблицу с автомобилями. Размер таблицы регулируется через параметр count

---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,29 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.config.models.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarsController {
+
+    @GetMapping(value = "/cars")
+    public String printWelcome(ModelMap model, @RequestParam(value = "count", defaultValue = "5") Integer count) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("BMW", "Black", 1));
+        cars.add(new Car("Mercedes", "White", 2));
+        cars.add(new Car("Audi", "Red", 3));
+        cars.add(new Car("Toyota", "Blue", 4));
+        cars.add(new Car("Nissan", "Green", 5));
+        if(count > cars.size()) {
+            count = cars.size();
+        }
+        model.addAttribute("cars", cars.subList(0, count));
+        return "cars";
+    }
+}
